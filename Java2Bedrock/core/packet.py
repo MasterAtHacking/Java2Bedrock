1e62ed811ba6e8dbd6e34a8efd8dc6e725249b8c
MasterAtHacking <marshall.dzh@gmail.com>
2026-07-31 12:18:54 +0000

    Remove/update whatever you changed

--- Java2Bedrock/core/packet.py
+++
@@ -1,7 +0,0 @@
-class Packet:
-    def __init__(self, packet_type, data):
-        self.type = packet_type
-        self.data = data
-
-    def __str__(self):
-        return f"{self.type}: {self.data}"
--- Java2Bedrock/core/translators/movement.py
+++
@@ -1,9 +0,0 @@
-def translate_movement(packet):
-
-    return {
-        "type": "move_player",
-        "x": packet.get("x"),
-        "y": packet.get("y"),
-        "z": packet.get("z")
-    }
-
